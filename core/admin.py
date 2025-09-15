91d113bb2fc94cb7933a4d0e2c9f666416a523bc
saulv99 <viracocheasaul@gmail.com>
2025-09-14 23:40:07 -0400

    roles y permisos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Usuario, Medico, Paciente, Administrador, Especialidad, Auto, Rol, MedicoEspecialidad, Permiso
 
+admin.site.register(Usuario)
+admin.site.register(Medico)
+admin.site.register(Paciente)
+admin.site.register(Administrador)
+admin.site.register(Especialidad)
+admin.site.register(Auto)
+admin.site.register(Permiso)
+admin.site.register(Rol)
+admin.site.register(MedicoEspecialidad)
 # Register your models here.
--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
+class PermisoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permiso
+        fields = ['id', 'nombre', 'codigo', 'descripcion']
+from rest_framework import serializers
+from .models import *
+
 class RolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rol
